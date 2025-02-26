45c6a70c1f5fbef26a8127988ea63d063ac0b147
mitchelljae <184208425+mitchelljae@users.noreply.github.com>
2025-02-26 14:11:26 +0000

    Cleanup of files

--- templates/header.js
+++
@@ -1,9 +0,0 @@
-import { html } from "lighterhtml";
-import settings from "../settings.json";
-
-export const renderHeader = html.node`<div>
-  <div class='avatar-container'>
-    <img src='${settings.avatarImage}' class='avatar' alt='${settings.name}' />
-  </div>
-  <h1>${settings.name}</h1>
-</div>`;
