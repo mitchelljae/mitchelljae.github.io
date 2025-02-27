4a66a38e3c1c2a54b3b247a648fb7ac943b95417
mitchelljae <184208425+mitchelljae@users.noreply.github.com>
2025-02-27 02:53:22 +0000

    Update footer.js

--- templates/footer.js
+++ templates/footer.js
@@ -3,7 +3,7 @@ import settings from "../settings.json";
 
 export const renderFooter = html.node`<footer class="footer">
   <div class="tagline">
-    <b>Glitch in Biooo</b><br />
+    <b>Glitch in Bio</b><br />
     Your links, your way.
   </div>
   <a
