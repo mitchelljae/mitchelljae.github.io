98bebdb2b23f04922206cd6085431e7eebb53d66
mitchelljae <184208425+mitchelljae@users.noreply.github.com>
2025-03-02 05:19:56 +0000

    Update footer.js

--- templates/footer.js
+++ templates/footer.js
@@ -4,7 +4,7 @@ import settings from "/settings.js?import";
 export const renderFooter = html.node`<hr class="hr-footer" data-content="X">
 <footer class="footer">
   <div class="tagline">
-    Based on <a target="_top" href="https://glitch.com/edit/#!/remix/glitch-in-bio"
+    Based on the excellent <a target="_top" href="https://glitch.com/edit/#!/remix/glitch-in-bio"
   >Glitch in Bio.</a>
   </div>
 </footer>
