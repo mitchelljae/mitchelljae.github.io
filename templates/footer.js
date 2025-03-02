b62477edcee4a5ebb26181824855966fb712ea56
mitchelljae <184208425+mitchelljae@users.noreply.github.com>
2025-03-02 01:48:02 +0000

    Update footer.js

--- templates/footer.js
+++ templates/footer.js
@@ -1,8 +1,8 @@
 import { html } from "https://unpkg.com/lighterhtml?module";
 import settings from "/settings.js?import";
 
-export const renderFooter = html.node`<footer class="footer">
-  <hr class="hr-footer" data-content="X">
+export const renderFooter = html.node`<hr class="hr-footer" data-content="X">
+<footer class="footer">
   <div class="tagline">
     Based on <a target="_top" href="https://glitch.com/edit/#!/remix/glitch-in-bio"
   >Glitch in Bio.</a>
