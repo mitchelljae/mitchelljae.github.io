e0cd5f942833f66b1bb3ea383312332f5a78a99f
mitchelljae <184208425+mitchelljae@users.noreply.github.com>
2025-03-03 22:28:16 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,20 +7,20 @@ export const links = [
   {
     url: "https://twitch.tv/mitchelljae",
     header: "Twitch",
-    text: "Live streams, reactions, and the odd game",
+    text: "Live streams, reactions, and maybe the odd game",
     img: "https://cdn.glitch.me/efc5414a-882b-4708-af81-8461abbc1a82%2Fglitchmidblues.png?v=1634289872484",
     altText: "Glitch blue generative art"
   },
   {
-    url: "https://glitch.com/edit/#!/glitch-in-bio?path=README.md",
+    url: "https://bsky.app/profile/mitchelljae.bsky.social",
     header: "Bluesky",
-    text: "View source! Check out the code to see how it's built.",
+    text: "Rarely post, but better than the alternatives",
     img: ""
   },
   {
-    url: "https://www.glitch.com/",
+    url: "https://www.youtube.com/@mitchelljae",
     header: "Youtube",
-    text: "Built by and on Glitch, the easiest way to build the web.",
+    text: "Nothing there as yet, but I need content here",
     img: "https://cdn.glitch.me/efc5414a-882b-4708-af81-8461abbc1a82%2Fglitchdarkpinks.png?v=1634289859236"
   }
 ];
